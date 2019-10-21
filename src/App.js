1b6b2274a3df270ca0970b762946fc03aa757c05
yu46 <nasu.y@hotmail.co.jp>
2019-10-21 10:25:43 +0900

    learn jsx  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+
     return (
-      <div>
-        <h1>hello, world!</h1>
-      </div>
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input id="bar" type="text" onChange={() => { console.log("hi") }} />
+      </React.Fragment>
     );
   }
 }
